0c7c052af42bf8b7d7581c4b6045f28ff940c027
Kevin <kevin.k1252@gmail.com>
2025-06-11 11:15:59 +0200

    fix

--- ext/ext.cpp
+++ ext/ext.cpp
@@ -175,7 +175,7 @@ extern "C" VALUE rb_invertImage(VALUE self, VALUE rb_mat) {
 extern "C" VALUE rb_findContours(VALUE self, VALUE rb_mat, VALUE rb_retrieve_type) {
   Mat mat = convertRubyStringToMat(rb_mat);
 
-  // cvtColor(mat, mat, COLOR_BGR2GRAY);
+  cvtColor(mat, mat, COLOR_BGR2GRAY);
   // mat = Scalar(255) - mat;
 
   threshold(mat, mat, 40, 255, THRESH_BINARY);
